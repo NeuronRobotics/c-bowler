f731d08f80aa812fefc3d3e9e54df861219debab
Kevin Harrington <mad.hephaestus@gmail.com>
2012-01-04 21:37:17 +0000

    Mplab X project

---
+++ firmware/library/NR-Clib/release/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default
+CND_ARTIFACT_NAME_default=nr-clibrelease.x.a
+CND_ARTIFACT_PATH_default=dist/default/nr-clibrelease.x.a
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=NR-ClibRelease.X.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/NR-ClibRelease.X.tar
