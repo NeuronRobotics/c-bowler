0ca823c84c3384f9445e442f781dd9bc9bdc11db
Kevin Harrington <mad.hephaestus@gmail.com>
2012-01-23 18:18:35 +0000

    Adding Build date to XML

--- firmware/library/NR-Clib/development/toolchain/pic32/toolchain.mk
+++ firmware/library/NR-Clib/development/toolchain/pic32/toolchain.mk
@@ -3,7 +3,6 @@ PICTOOLCHAIN    = /home/opt/microchip/mplabc32/v2.02/bin/
 GCCP			=$(PICTOOLCHAIN)pic32-gcc  -O3 -mips16 -s
 ARP				=$(PICTOOLCHAIN)pic32-ar
 BIN2HEX			=$(PICTOOLCHAIN)pic32-bin2hex
-
 #PICTOOLCHAIN    = /usr/local/bin/
 #GCCP			=$(PICTOOLCHAIN)pic32mx-gcc  -O3 -mips16 -s
 #ARP				=$(PICTOOLCHAIN)pic32mx-ar
