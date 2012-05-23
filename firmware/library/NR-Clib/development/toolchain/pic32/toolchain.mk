248ac1bfbb30eded014bdc4e47071167c26c6cad
Kevin Harrington <mad.hephaestus@gmail.com>
2012-05-23 23:06:18 +0000

    new toolchain

--- firmware/library/NR-Clib/development/toolchain/pic32/toolchain.mk
+++ firmware/library/NR-Clib/development/toolchain/pic32/toolchain.mk
@@ -1,6 +1,6 @@
 #http://ww1.microchip.com/downloads/mplab/X_Beta/installer.html
 PICTOOLCHAIN    = /home/opt/microchip/xc32/v1.00/bin/
-GCCP			=$(PICTOOLCHAIN)xc32-gcc  -O3 -mips16 -s
+GCCP			=$(PICTOOLCHAIN)xc32-gcc  -O1 -mips16 -s
 ARP				=$(PICTOOLCHAIN)xc32-ar
 BIN2HEX			=$(PICTOOLCHAIN)xc32-bin2hex
 #PICTOOLCHAIN    = /usr/local/bin/
