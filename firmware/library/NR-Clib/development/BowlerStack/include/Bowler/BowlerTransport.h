9d4f1626b2beb27125526f62cc76223bed9449b9
Kevin Harrington <mad.hephaestus@gmail.com>
2014-02-14 23:39:17 -0500

    added missing include

--- firmware/library/NR-Clib/development/BowlerStack/include/Bowler/BowlerTransport.h
+++ firmware/library/NR-Clib/development/BowlerStack/include/Bowler/BowlerTransport.h
@@ -7,6 +7,8 @@
 
 #ifndef BOWLERTRANSPORT_H_
 #define BOWLERTRANSPORT_H_
+
+#include "FIFO.h"
 BOOL GetBowlerPacket_arch(BowlerPacket * Packet);
 BOOL GetBowlerPacket(BowlerPacket * Packet,BYTE_FIFO_STORAGE * fifo);
 BOOL PutBowlerPacket(BowlerPacket * Packet);
