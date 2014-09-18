8d2a275c171c00b8110049cd207a7a7f3214071d
madhephaestus <mad.hephaestus@gmail.com>
2014-09-17 23:32:11 -0400

    packing structs

--- toolchain/pic32/toolchain.mk
+++ toolchain/pic32/toolchain.mk
@@ -13,7 +13,7 @@ else
 
 endif
 
-GCCP			=$(PICTOOLCHAIN)xc32-gcc -mips16 -O1 -s 
+GCCP			=$(PICTOOLCHAIN)xc32-gcc -mips16 -O1 -pack-struct -s 
 ARP				=$(PICTOOLCHAIN)xc32-ar
 BIN2HEX			=$(PICTOOLCHAIN)xc32-bin2hex
 #PICTOOLCHAIN    = /usr/local/bin/
