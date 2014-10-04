5ad930756baa69e60d6b5c6fee643e6e2e3b2c2c
madhephaestus <mad.hephaestus@gmail.com>
2014-10-04 16:11:14 -0400

    ok, bootloader fits with fuull sized rpc definitions

--- BowlerStack/include/Bowler/namespace.h
+++ BowlerStack/include/Bowler/namespace.h
@@ -21,11 +21,11 @@ typedef struct  _RPC_LIST{
 	//This is the callback function pointer for execution of the method
 	packetEventCallback * callback;
         //This is the array of argument data types
-	const char  arguments [14 ];
+	const char  arguments [22 ];
         //This is the bowler method for this RPC
 	uint8_t responseMethod;
         //This is the array of argument data types
-	const char responseArguments[14 ];
+	const char responseArguments[22];
         
 	//This is the linked list field
 	struct _RPC_LIST * next;
